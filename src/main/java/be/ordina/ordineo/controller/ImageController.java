de809c2658512b8b1c741849fd86f8beba7ccf5c
Derya <derya.duru@ordina.be>
2016-03-23 17:46:23 +0100

    Added integration tests

--- src/main/java/be/ordina/ordineo/controller/ImageController.java
+++ src/main/java/be/ordina/ordineo/controller/ImageController.java
@@ -4,13 +4,16 @@ import be.ordina.ordineo.model.Image;
 import be.ordina.ordineo.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by DeDu on 17/03/2016.
@@ -23,17 +26,17 @@ public class ImageController {
     private ImageRepository imageRepository;
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/upload")
-    public void uploadImage(@RequestParam("file") MultipartFile image) {
-        try {
-            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("image-core-service/src/main/resources/images/" + image.getOriginalFilename())));
-            FileCopyUtils.copy(image.getInputStream(), stream);
-            stream.close();
-        }
-        catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
+//    @RequestMapping(method = RequestMethod.POST, value = "/upload")
+//    public void uploadImage(@RequestParam("file") MultipartFile image) {
+//        try {
+//            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("image-core-service/src/main/resources/images/" + image.getOriginalFilename())));
+//            FileCopyUtils.copy(image.getInputStream(), stream);
+//            stream.close();
+//        }
+//        catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//    }
 
     @RequestMapping(method = RequestMethod.GET, value = ("/{username}"), produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     public byte[] getImage(@PathVariable String username) throws IllegalArgumentException, NullPointerException, IOException{
